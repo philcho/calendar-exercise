3f64f53a3f8c3cc59ea44bc5237a23839cc26a8c
philcho <Phil Cho>
2018-03-12 13:05:33 -0700

    [JS/HTML] Add ARIA attributes to event detail overlay.

--- src/components/EventDetailOverlay.js
+++ src/components/EventDetailOverlay.js
@@ -24,17 +24,14 @@ export default class EventDetailOverlay extends PureComponent {
 
         let displayDateTime = `${displayDate} ${startHourDisplay} - ${endHourDisplay}`;
 
-        // TODO: The event label color should match the event color
-        // TODO: Add appropriate ARIA tags to overlay/dialog
-        // TODO: Support clicking outside of the overlay to close it
-        // TODO: Support clicking ESC to close it
         return (
-            <section className="event-detail-overlay">
-                <div className="event-detail-overlay__container">
+            <section className="event-detail-overlay" role="dialog">
+                <div className="event-detail-overlay__container" role="document">
                     <button
                         className="event-detail-overlay__close"
                         title="Close detail view"
                         onClick={onClose}
+                        aria-label="Close"
                     />
                     <div>
                         {displayDateTime}
